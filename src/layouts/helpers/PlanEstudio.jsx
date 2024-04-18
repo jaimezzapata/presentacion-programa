b0845a1b961b31e9b7aff623c235cddc0af0dc7a
jaimezzapata <zapataval2304@gmail.com>
2024-04-18 09:42:43 -0500

    Se finaliza cuadricula de plan de estudio

--- src/layouts/helpers/PlanEstudio.jsx
+++ src/layouts/helpers/PlanEstudio.jsx
@@ -1,40 +1,75 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const PlanEstudio = () => {
   return (
-    <div class="parent">
-      <div class="div1">
+    <div className="parent">
+      <div className="div1">
         <h2>Tipo Competencia</h2>
       </div>
-      <div class="div2">
+      <div className="div2">
         <h2>Módulos</h2>
       </div>
-      <div class="div3">
+      <div className="div3">
         <h2>Nivel I</h2>
       </div>
-      <div class="div4">
+      <div className="div4">
         <h2>Nivel II</h2>
       </div>
-      <div class="div5">
+      <div className="div5">
         <h2>Nivel III</h2>
       </div>
-      <div class="div6"> </div>
-      <div class="div7"> </div>
-      <div class="div8"> </div>
-      <div class="div9"> </div>
-      <div class="div10"> </div>
-      <div class="div11"> </div>
-      <div class="div12"> </div>
-      <div class="div13"> </div>
-      <div class="div14"> </div>
-      <div class="div15"> </div>
-      <div class="div16"> </div>
-      <div class="div17"> </div>
-      <div class="div18"> </div>
-      <div class="div19"> </div>
-      <div class="div20"> </div>
-      <div class="div21"> </div>
+      <div className="div6">
+        <p>Modulos de programación</p>
+      </div>
+      <div className="div7">
+        <p>Módulo de Movilidad</p>
+      </div>
+      <div className="div8">
+        <p>Obligatorias del programa</p>
+      </div>
+      <div className="div9">
+        <p>Módulo Web</p>
+      </div>
+      <div className="div10">
+        <p>Módulo Comun</p>
+      </div>
+      <div className="div11">
+        <p>Obligatorias Intitucionales</p>
+      </div>
+      <div className="div12">
+        <Link>Lógica de programación</Link>
+      </div>
+      <div className="div13">
+        <Link>Metodologías Ágiles</Link>
+      </div>
+      <div className="div14">
+        <Link>Introducción a la programación</Link>
+      </div>
+      <div className="div15">
+        <Link>Cátedra ser emprendedor</Link>
+      </div>
+      <div className="div16">
+        <Link>Programación de aplicaciones moviles 1</Link>
+      </div>
+      <div className="div17">
+        <Link>Programación para la Web 1</Link>
+      </div>
+      <div className="div18">
+        <Link>Gestión de bases de datos</Link>
+      </div>
+      <div className="div19">
+        <Link>Nuevas tecnologías de la programación</Link>
+      </div>
+      <div className="div20">
+        <Link>Programación de aplicaciones moviles 2</Link>
+      </div>
+      <div className="div21">
+        <Link>Programación para la Web 2</Link>
+      </div>
+      <Link to='/' className='btn-continuar'>Home</Link>
     </div>
+    
   )
 }
 
